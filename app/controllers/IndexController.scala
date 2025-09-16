c126df140e592c0815d593d4621bd1227c62f27f
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2025-09-16 17:00:11 +0100

    CARF-214 formatting

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -22,7 +22,6 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.auth.core.AffinityGroup
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.IndexView
 
 import javax.inject.Inject
 import scala.concurrent.Future

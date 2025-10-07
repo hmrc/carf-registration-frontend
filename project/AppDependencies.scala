a9d90dbd5252a3de8d270102a031a6cdd9108fad
AishaBi <43678132+AishaBi@users.noreply.github.com>
2025-10-07 14:14:03 +0100

    .

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion        = "9.19.0"
-  private val hmrcMongoVersion        = "2.7.0"
-  private val playFrontendHmrcVersion = "12.16.0"
+  private val hmrcMongoVersion        = "2.8.0"
+  private val playFrontendHmrcVersion = "12.17.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,

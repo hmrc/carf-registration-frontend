d4717644319b1289d453dd4f4f291a8a197ab0a3
periklas <1190980+periklas@users.noreply.github.com>
2025-11-03 14:25:17 +0000

    CARF-165 | Fixed typo in messages

--- app/controllers/WhatIsYourNameIndividualController.scala
+++ app/controllers/WhatIsYourNameIndividualController.scala
@@ -18,8 +18,6 @@ package controllers
 
 import controllers.actions.*
 import forms.WhatIsYourNameIndividualFormProvider
-
-import javax.inject.Inject
 import models.{Mode, Name}
 import navigation.Navigator
 import pages.WhatIsYourNameIndividualPage
@@ -30,6 +28,7 @@ import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.WhatIsYourNameIndividualView
 
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class WhatIsYourNameIndividualController @Inject() (

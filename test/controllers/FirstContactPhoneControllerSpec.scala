5904352b93663d793935fea89109e5ab9ac2bbd7
AishaBi <43678132+AishaBi@users.noreply.github.com>
2025-11-07 16:14:03 +0000

    .

--- test/controllers/FirstContactPhoneControllerSpec.scala
+++ test/controllers/FirstContactPhoneControllerSpec.scala
@@ -23,7 +23,7 @@ import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.FirstContactPhonePage
+import pages.{FirstContactNamePage, FirstContactPhonePage}
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
@@ -63,7 +63,7 @@ class FirstContactPhoneControllerSpec extends SpecBase with MockitoSugar {
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(FirstContactPhonePage, true).success.value
+      val userAnswers = userAnswersWithName.set(FirstContactPhonePage, true).success.value
 
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
@@ -75,7 +75,10 @@ class FirstContactPhoneControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result)          mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form.fill(true), NormalMode, "Timothy")(
+          request,
+          messages(application)
+        ).toString
       }
     }
 
@@ -117,7 +120,10 @@ class FirstContactPhoneControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result)          mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, NormalMode, "Timothy")(
+          request,
+          messages(application)
+        ).toString
       }
     }
 

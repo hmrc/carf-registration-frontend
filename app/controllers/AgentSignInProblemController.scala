65a09963be607f06f89d7de45011b0c262fbe388
periklas <1190980+periklas@users.noreply.github.com>
2025-11-21 16:08:34 +0000

    CARF-182 | Removed unused imports in AgentSignInProblemController

--- app/controllers/AgentSignInProblemController.scala
+++ app/controllers/AgentSignInProblemController.scala
@@ -16,8 +16,6 @@
 
 package controllers
 
-import config.FrontendAppConfig
-import controllers.actions.*
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController

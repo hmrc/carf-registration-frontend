54cbf7f48fffada12dc2271b49bea910d0ab0ac0
periklas <1190980+periklas@users.noreply.github.com>
2025-11-26 14:34:30 +0000

    CARF-257 | Added app/utils/CheckYourAnswersValidator file

--- test/controllers/InformationMissingControllerSpec.scala
+++ test/controllers/InformationMissingControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
@@ -20,8 +36,11 @@ class InformationMissingControllerSpec extends SpecBase {
 
         val view = application.injector.instanceOf[InformationMissingView]
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        status(result)          mustEqual OK
+        contentAsString(result) mustEqual view(routes.IndexController.onPageLoad().url)(
+          request,
+          messages(application)
+        ).toString
       }
     }
   }

bd8f5204e85ca6cc190ab583de6a00ab7b72b448
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-02-06 14:47:39 +0000

    SubmissionLockAction

---
+++ app/controllers/actions/SubmissionLockAction.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+import models.requests.OptionalDataRequest
+import pages.SubmissionSucceededPage
+
+@Singleton
+class SubmissionLockAction @Inject() (
+    val parser: BodyParsers.Default
+)(implicit ec: ExecutionContext)
+    extends ActionFilter[OptionalDataRequest] {
+
+  // Paths still allowed after successful submission
+  private val allowedPaths: Set[String] = Set(
+    "/register/confirm-registration",
+    "/problem/page-unavailable",
+    "/account/sign-out-survey",
+    "/account/sign-out",
+    "/account/signed-out"
+  )
+
+  override protected def executionContext: ExecutionContext = ec
+
+  override protected def filter[A](request: OptionalDataRequest[A]): Future[Option[Result]] = {
+    val path = request.path
+
+    if (allowedPaths.exists(path.startsWith)) {
+      Future.successful(None)
+    } else {
+      val submitted = request.userAnswers match {
+        case Some(ua) => ua.get(SubmissionSucceededPage).contains(true)
+        case None     => false
+      }
+
+      if (submitted) {
+        Future.successful(
+          Some(Results.Redirect(controllers.routes.PageUnavailableController.onPageLoad()))
+        )
+      } else {
+        Future.successful(None)
+      }
+    }
+  }
+}

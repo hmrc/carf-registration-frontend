816df958fd460b358aae389c1631d1f0c4bf83c2
periklas <1190980+periklas@users.noreply.github.com>
2026-02-23 11:13:50 +0000

    CARF-364 | Added SubscriptionHelper and moved CreateSubscriptionRequest logic from subscriptionService

--- app/services/SubscriptionService.scala
+++ app/services/SubscriptionService.scala
@@ -16,127 +16,43 @@
 
 package services
 
-import cats.implicits.*
 import connectors.SubscriptionConnector
-import models.JourneyType.*
 import models.error.ApiError
-import models.requests.{ContactInformation, CreateSubscriptionRequest, IndividualDetails, OrganisationDetails}
-import models.{IdentifierType, JourneyType, Name, SubscriptionId, UserAnswers}
-import pages.*
-import pages.individual.{IndividualEmailPage, IndividualPhoneNumberPage, NiNumberPage, WhatIsYourNameIndividualPage}
-import pages.individualWithoutId.{IndFindAddressPage, IndWithoutIdAddressPage, IndWithoutNinoNamePage}
-import pages.orgWithoutId.TradingNamePage
-import pages.organisation.*
+import models.requests.CreateSubscriptionRequest
+import models.{SubscriptionId, UserAnswers}
 import play.api.Logging
-import uk.gov.hmrc.auth.core.AffinityGroup
 import uk.gov.hmrc.http.HeaderCarrier
+import utils.SubscriptionHelper
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class SubscriptionService @Inject() (subscriptionConnector: SubscriptionConnector) extends Logging {
+class SubscriptionService @Inject() (
+    subscriptionConnector: SubscriptionConnector,
+    subscriptionHelper: SubscriptionHelper
+) extends Logging {
 
   def subscribe(userAnswers: UserAnswers)(implicit
       hc: HeaderCarrier,
       ec: ExecutionContext
-  ): Future[Either[ApiError, SubscriptionId]] =
-    subscriptionConnector
-      .createSubscription(buildSubscriptionRequest(userAnswers))
-      .map { result =>
-        logger.info(s"Subscription has been created successfully: $result")
-        result
-      }
-      .leftMap { error =>
-        logger.error(s"Failed to create subscription: $error")
-        error
-      }
-      .value
+  ): Future[Either[ApiError, SubscriptionId]] = {
+    val maybeRequest: Option[CreateSubscriptionRequest] = subscriptionHelper.buildSubscriptionRequest(userAnswers)
 
-  private def buildSubscriptionRequest(userAnswers: UserAnswers): CreateSubscriptionRequest = {
-    val safeId = "safeid"
-
-    CreateSubscriptionRequest(
-      idType = IdentifierType.SAFE,
-      idNumber = safeId,
-      tradingName = getTradingName(userAnswers),
-      gbUser = isGBUser(userAnswers),
-      primaryContact = buildPrimaryContact(userAnswers),
-      secondaryContact = buildSecondaryContact(userAnswers)
-    )
+    maybeRequest match {
+      case Some(request) =>
+        subscriptionConnector
+          .createSubscription(request)
+          .value
+          .map {
+            case Right(result) => Right(result)
+            case Left(error)   =>
+              logger.error(s"Failed to create subscription: $error")
+              Left(error)
+          }
+      case None          =>
+        logger.error("There has been an error building the subscription request from user answers")
+        Future.successful(Left(ApiError.BadRequestError))
+    }
   }
-
-  private def buildPrimaryContact(userAnswers: UserAnswers): ContactInformation =
-    userAnswers.journeyType match {
-      case Some(IndWithNino)  => buildIndividualContact(userAnswers, WhatIsYourNameIndividualPage)
-      case Some(IndWithoutId) => buildIndividualContact(userAnswers, IndWithoutNinoNamePage)
-      case Some(IndWithUtr)   => buildIndividualContact(userAnswers, WhatIsYourNamePage)
-      case _                  => buildOrganisationContact(userAnswers, isPrimary = true)
-    }
-
-  private def buildIndividualContact(
-      userAnswers: UserAnswers,
-      namePage: QuestionPage[Name]
-  ): ContactInformation =
-    ContactInformation(
-      individual = userAnswers.get(namePage).map(name => IndividualDetails(name.firstName, name.lastName)),
-      organisation = None,
-      email = userAnswers.get(IndividualEmailPage).fold("")(identity),
-      phone = userAnswers.get(IndividualPhoneNumberPage)
-    )
-
-  private def buildOrganisationContact(
-      userAnswers: UserAnswers,
-      isPrimary: Boolean
-  ): ContactInformation =
-    if (isPrimary) {
-      ContactInformation(
-        individual = None,
-        organisation = userAnswers.get(FirstContactNamePage).map(OrganisationDetails(_)),
-        email = userAnswers.get(FirstContactEmailPage).fold("")(identity),
-        phone = userAnswers.get(FirstContactPhoneNumberPage)
-      )
-    } else {
-      ContactInformation(
-        individual = None,
-        organisation = userAnswers.get(OrganisationSecondContactNamePage).map(OrganisationDetails(_)),
-        email = userAnswers.get(OrganisationSecondContactEmailPage).fold("")(identity),
-        phone = userAnswers.get(OrganisationSecondContactPhoneNumberPage)
-      )
-    }
-
-  private def buildSecondaryContact(userAnswers: UserAnswers): Option[ContactInformation] =
-    if (isOrganisationJourney(userAnswers.journeyType) && hasSecondContact(userAnswers)) {
-      Some(buildOrganisationContact(userAnswers, isPrimary = false))
-    } else {
-      None
-    }
-
-  private def isOrganisationJourney(journeyType: Option[JourneyType]): Boolean =
-    journeyType match {
-      case Some(OrgWithUtr) | Some(OrgWithoutId) => true
-      case _                                     => false
-    }
-
-  private def hasSecondContact(userAnswers: UserAnswers): Boolean =
-    userAnswers.get(OrganisationHaveSecondContactPage).contains(true)
-
-  private def getTradingName(userAnswers: UserAnswers): Option[String] =
-    userAnswers.journeyType match {
-      case Some(OrgWithoutId) => userAnswers.get(TradingNamePage)
-      case Some(OrgWithUtr)   => userAnswers.get(WhatIsTheNameOfYourBusinessPage)
-      case _                  => None
-    }
-
-  private def isGBUser(userAnswers: UserAnswers): Boolean = {
-    val businessHasUtr              =
-      userAnswers.get(UniqueTaxpayerReferenceInUserAnswers).exists(_.uniqueTaxPayerReference.trim.nonEmpty)
-    val individualHasNino           = userAnswers.get(NiNumberPage).exists(_.trim.nonEmpty)
-    val individualAddressLookupIsGb = userAnswers.get(IndFindAddressPage).nonEmpty
-    val individualManualAddressIsGb = userAnswers.get(IndWithoutIdAddressPage).exists(_.countryCode == "GB")
-    val registeredAddressIsGb       = userAnswers.get(RegisteredAddressInUkPage).contains(true)
-
-    businessHasUtr || individualHasNino || individualAddressLookupIsGb || individualManualAddressIsGb || registeredAddressIsGb
-  }
-
 }

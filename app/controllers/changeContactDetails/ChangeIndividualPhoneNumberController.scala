40363aae95aaefa6bbb9981dd4117bbe15591e24
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-04-01 10:54:30 +0100

    compile errors fix

--- app/controllers/changeContactDetails/ChangeIndividualPhoneNumberController.scala
+++ app/controllers/changeContactDetails/ChangeIndividualPhoneNumberController.scala
@@ -1,10 +1,27 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.changeContactDetails
 
-import controllers.actions.*
+import controllers.actions.{CarfIdRetrievalAction, ChangeDetailsDataRequiredAction}
 import forms.individual.IndividualPhoneNumberFormProvider
-import models.Mode
+import models.NormalMode
 import navigation.Navigator
 import pages.changeContactDetails.ChangeDetailsIndividualPhoneNumberPage
+import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
@@ -14,41 +31,39 @@ import views.html.ChangeIndividualPhoneNumberView
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class ChangeIndividualPhoneNumberController @Inject()(
-                                        override val messagesApi: MessagesApi,
-                                        sessionRepository: SessionRepository,
-                                        navigator: Navigator,
-                                        identify: IdentifierAction,
-                                        getData: DataRetrievalAction,
-                                        submissionLock: SubmissionLockAction,
-                                        requireData: DataRequiredAction,
-                                        formProvider: IndividualPhoneNumberFormProvider,
-                                        val controllerComponents: MessagesControllerComponents,
-                                        view: ChangeIndividualPhoneNumberView
-                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+class ChangeIndividualPhoneNumberController @Inject() (
+    override val messagesApi: MessagesApi,
+    sessionRepository: SessionRepository,
+    navigator: Navigator,
+    carfIdRetrieval: CarfIdRetrievalAction,
+    changeDetailsDataRequiredAction: ChangeDetailsDataRequiredAction,
+    formProvider: IndividualPhoneNumberFormProvider,
+    val controllerComponents: MessagesControllerComponents,
+    view: ChangeIndividualPhoneNumberView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
 
-  val form = formProvider()
+  val form: Form[String] = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify() andThen getData() andThen submissionLock andThen requireData) {
+  def onPageLoad(): Action[AnyContent] = (carfIdRetrieval() andThen changeDetailsDataRequiredAction) {
     implicit request =>
-
       val preparedForm = request.userAnswers.get(ChangeDetailsIndividualPhoneNumberPage).fold(form)(form.fill)
 
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify() andThen getData() andThen requireData).async {
+  def onSubmit(): Action[AnyContent] = (carfIdRetrieval() andThen changeDetailsDataRequiredAction).async {
     implicit request =>
-
-      form.bindFromRequest().fold(
-        formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
-
-        value =>
-          for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(ChangeDetailsIndividualPhoneNumberPage, value))
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(ChangeIndividualPhoneNumberPage, mode, updatedAnswers))
-      )
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(ChangeDetailsIndividualPhoneNumberPage, value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(ChangeDetailsIndividualPhoneNumberPage, NormalMode, updatedAnswers))
+        )
   }
 }

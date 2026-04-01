0927f4e4afa732755b05cc1719d517470a7331a0
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-04-01 13:10:26 +0100

    tests

--- app/navigation/NormalRoutesNavigator.scala
+++ app/navigation/NormalRoutesNavigator.scala
@@ -152,8 +152,8 @@ trait NormalRoutesNavigator extends UserAnswersHelper with Logging {
 
     case ChangeDetailsIndividualPhoneNumberPage =>
       _ => changeDetailsRoutes.ChangeIndividualContactDetailsController.onPageLoad()
-      
-    case _                                =>
+
+    case _ =>
       _ => routes.JourneyRecoveryController.onPageLoad()
   }
 
--- test/controllers/changeContactDetails/ChangeIndividualPhoneNumberControllerSpec.scala
+++ test/controllers/changeContactDetails/ChangeIndividualPhoneNumberControllerSpec.scala
@@ -1,13 +1,31 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.changeContactDetails
 
 import base.SpecBase
-import controllers.routes
-import models.{NormalMode, UserAnswers}
+import controllers.{changeContactDetails, routes}
+import forms.individual.IndividualPhoneNumberFormProvider
+import models.UserAnswers
 import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.ChangeIndividualPhoneNumberPage
+import pages.changeContactDetails.ChangeDetailsIndividualPhoneNumberPage
+import play.api.data.Form
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
@@ -19,14 +37,13 @@ import scala.concurrent.Future
 
 class ChangeIndividualPhoneNumberControllerSpec extends SpecBase with MockitoSugar {
 
-  val formProvider = new ChangeIndividualPhoneNumberFormProvider()
-  val form = formProvider()
+  val formProvider: IndividualPhoneNumberFormProvider = new IndividualPhoneNumberFormProvider()
+  val form: Form[String]                              = formProvider()
+  def onwardRoute: Call                               = Call("GET", "/foo")
+  val validPhoneNumber: String                        = "07777777777"
 
-  def onwardRoute = Call("GET", "/foo")
-
-  val validAnswer = 1
-
-  lazy val changeIndividualPhoneNumberRoute = routes.ChangeIndividualPhoneNumberController.onPageLoad(NormalMode).url
+  lazy val changeIndividualPhoneNumberRoute: String =
+    changeContactDetails.routes.ChangeIndividualPhoneNumberController.onPageLoad().url
 
   "ChangeIndividualPhoneNumber Controller" - {
 
@@ -36,31 +53,27 @@ class ChangeIndividualPhoneNumberControllerSpec extends SpecBase with MockitoSug
 
       running(application) {
         val request = FakeRequest(GET, changeIndividualPhoneNumberRoute)
+        val result  = route(application, request).value
+        val view    = application.injector.instanceOf[ChangeIndividualPhoneNumberView]
 
-        val result = route(application, request).value
-
-        val view = application.injector.instanceOf[ChangeIndividualPhoneNumberView]
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+        status(result)          mustEqual OK
+        contentAsString(result) mustEqual view(form)(request, messages(application)).toString
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
 
-      val userAnswers = UserAnswers(userAnswersId).set(ChangeIndividualPhoneNumberPage, validAnswer).success.value
-
+      val userAnswers =
+        UserAnswers(userAnswersId).set(ChangeDetailsIndividualPhoneNumberPage, validPhoneNumber).success.value
       val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
 
       running(application) {
         val request = FakeRequest(GET, changeIndividualPhoneNumberRoute)
+        val view    = application.injector.instanceOf[ChangeIndividualPhoneNumberView]
+        val result  = route(application, request).value
 
-        val view = application.injector.instanceOf[ChangeIndividualPhoneNumberView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(validAnswer), NormalMode)(request, messages(application)).toString
+        status(result)          mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(validPhoneNumber))(request, messages(application)).toString
       }
     }
 
@@ -77,11 +90,11 @@ class ChangeIndividualPhoneNumberControllerSpec extends SpecBase with MockitoSug
       running(application) {
         val request =
           FakeRequest(POST, changeIndividualPhoneNumberRoute)
-            .withFormUrlEncodedBody(("value", validAnswer.toString))
+            .withFormUrlEncodedBody(("value", validPhoneNumber))
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
+        status(result)                 mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual onwardRoute.url
       }
     }
@@ -96,13 +109,11 @@ class ChangeIndividualPhoneNumberControllerSpec extends SpecBase with MockitoSug
             .withFormUrlEncodedBody(("value", "invalid value"))
 
         val boundForm = form.bind(Map("value" -> "invalid value"))
+        val view      = application.injector.instanceOf[ChangeIndividualPhoneNumberView]
+        val result    = route(application, request).value
 
-        val view = application.injector.instanceOf[ChangeIndividualPhoneNumberView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        status(result)          mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm)(request, messages(application)).toString
       }
     }
 
@@ -112,10 +123,9 @@ class ChangeIndividualPhoneNumberControllerSpec extends SpecBase with MockitoSug
 
       running(application) {
         val request = FakeRequest(GET, changeIndividualPhoneNumberRoute)
+        val result  = route(application, request).value
 
-        val result = route(application, request).value
-
-        status(result) mustEqual SEE_OTHER
+        status(result)                 mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }
@@ -127,12 +137,11 @@ class ChangeIndividualPhoneNumberControllerSpec extends SpecBase with MockitoSug
       running(application) {
         val request =
           FakeRequest(POST, changeIndividualPhoneNumberRoute)
-            .withFormUrlEncodedBody(("value", validAnswer.toString))
+            .withFormUrlEncodedBody(("value", validPhoneNumber))
 
         val result = route(application, request).value
 
-        status(result) mustEqual SEE_OTHER
-
+        status(result)                 mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }

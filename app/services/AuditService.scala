b1b4377c9fba7ed94130038aec20c367a129aa8c
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2026-06-18 13:39:58 +0100

    formatting

--- app/services/AuditService.scala
+++ app/services/AuditService.scala
@@ -115,13 +115,12 @@ class AuditService @Inject (auditConnector: AuditConnector)(using ec: ExecutionC
                            )
     } yield ()
 
-  private def convertToExtendedEvent(eventJsValue: JsValue, auditType: String) = {
+  private def convertToExtendedEvent(eventJsValue: JsValue, auditType: String) =
     ExtendedDataEvent(
       auditSource = "carf-registration-frontend",
       auditType = auditType,
       detail = eventJsValue
     )
-  }
 
   private def getUtrJourneyType(userAnswers: UserAnswers): Option[UtrJourneyAuditEvent] =
     (

b9f0a8cde1480e3251e6c745391250092864535c
maykenworthy <247179073+maykenworthy@users.noreply.github.com>
2026-09-09 11:35:57 +0100

    CARF-308: Added/Updated various problem pages and relevant tests

--- app/controllers/JourneyRecoveryController.scala
+++ app/controllers/JourneyRecoveryController.scala
@@ -16,38 +16,25 @@
 
 package controllers
 
+import config.FrontendAppConfig
 import controllers.actions.BasicAuthAction
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.binders.*
-import uk.gov.hmrc.play.bootstrap.binders.RedirectUrl.*
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.{JourneyRecoveryContinueView, JourneyRecoveryStartAgainView}
-import utils.LoggerUtil.*
+import views.html.problem.JourneyRecoveryView
 
 import javax.inject.Inject
 
 class JourneyRecoveryController @Inject() (
     val controllerComponents: MessagesControllerComponents,
     basicAuth: BasicAuthAction,
-    continueView: JourneyRecoveryContinueView,
-    startAgainView: JourneyRecoveryStartAgainView
+    view: JourneyRecoveryView,
+    frontendAppConfig: FrontendAppConfig
 ) extends FrontendBaseController
     with I18nSupport {
 
   def onPageLoad(continueUrl: Option[RedirectUrl] = None): Action[AnyContent] = basicAuth() { implicit request =>
-    val safeUrl: Option[String] = continueUrl.flatMap { unsafeUrl =>
-      unsafeUrl.getEither(OnlyRelative) match {
-        case Right(safeUrl) =>
-          Some(safeUrl.url)
-        case Left(message)  =>
-          logInfo(message)
-          None
-      }
-    }
-
-    safeUrl
-      .map(url => Ok(continueView(url)))
-      .getOrElse(Ok(startAgainView()))
+    Ok(view(frontendAppConfig.aeoiEmailAddress))
   }
 }
